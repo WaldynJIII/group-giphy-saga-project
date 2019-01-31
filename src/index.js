9e1ba5dd8f72d609c542e1a9526019b65946b7bd
Julie Zembik <kurokurosan@gmail.com>
2019-01-31 17:00:24 -0600

    added merge

--- src/index.js
+++ src/index.js
@@ -5,29 +5,6 @@ import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 
 import App from './components/App/App';
-<<<<<<< HEAD
-
-
-import createSagaMiddleware from 'redux-saga';
-import { takeEvery, put } from 'redux-saga/effects'
-import axios from 'axios'
-
-function* getFavorite() {
-    try {
-        yield axios.get('/api/favorite');
-        const nextAction = { type: 'GET_FAVORITES' }
-        yield put(nextAction);
-    } catch (error) {
-        console.log('ERROR in getfavorite', error);
-        alert('Something went wrong in getfavorite');
-    }
-}
-
-
-function* rootSaga() {
-    yield takeEvery('FETCH_GIPHY', getFavorite);
-    // yield takeEvery('POST_GIPHY', postFruitFetcher);
-=======
 
 import createSagaMiddleware from 'redux-saga';
 import { takeEvery, put } from 'redux-saga/effects'
@@ -37,7 +14,6 @@ function* rootSaga() {
     yield takeEvery('FETCH_GIPHY', firstGiph);
     yield takeEvery('POST_GIPHY', postGiph);
     yield takeEvery('FETCH_GIPHY', getCatGiph);
->>>>>>> f693e4c43752ffaf70a8c9fb18ba2dab35a72aa2
 }
 
 
@@ -52,32 +28,6 @@ const displayList = (state = [], action)=>{
 };
 
 
-<<<<<<< HEAD
-// function* firstSaga(action) {
-
-//     // replaces the need for .then and .catch
-//     try {
-//         const boxResponse = yield axios.get('/api/giphy');
-//         // same as dispatch
-//         console.log(boxResponse.data)
-//         const nextAction = { type: 'SET_GIPHY_DISPLAY', payload: boxResponse.data };
-//         yield put(nextAction); // trigger our reducer
-//     } catch (error) {
-//         console.log('Error making GET request');
-//         alert('there was a problem');
-//     }
-// }
-// function* postFruitFetcher(action) {
-//     try {
-//         yield axios.post('/api/plant', action.payload);
-//         const nextAction = { type: 'POST_GIPHY' };
-//         yield put(nextAction);
-//     } catch (error) {
-//         console.log('Error making POST request');
-//         alert('there was a problem. Check console logs');
-//     }
-// }
-=======
 function* getCatGiph(action) {
     try {
         const serverResponse = yield axios.get('/api/category');
@@ -116,7 +66,6 @@ function* postGiph(action) {
         alert('there was a problem. Check console logs');
     }
 }
->>>>>>> f693e4c43752ffaf70a8c9fb18ba2dab35a72aa2
 
 // Create sagaMiddleware
 const sagaMiddleware = createSagaMiddleware();
